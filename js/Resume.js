5608d65dbbbab404b60210b8972c59ff461aa672
agboulter <agboulter@gmail.com>
2025-10-23 20:50:11 +1100

    Add files via upload
    Re-upload of CSS and JS files

---
+++ js/Resume.js
@@ -0,0 +1,12 @@
+(function($) {
+    document.getElementById('downloadResume').onclick = function() {
+        const link = document.createElement('a');
+        link.href = 'assets/Ashleigh_Boulter_Resume.pdf'; 
+        link.download = 'Ashleigh_Boulter_Resume.pdf';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        
+        showCustomAlert("Your download will start shortly.");
+    };
+})(jQuery);
---
+++ js/formValidation.js
@@ -0,0 +1,18 @@
+
+function validateForm() {
+
+    const formValues = [
+        { id: 'contact-name', name: 'Name', message: 'Name must be filled out' },
+        { id: 'contact-email', name: 'Email', message: 'Email must be filled out' },
+        { id: 'contact-message', name: 'Message', message: 'Message must be filled out' }
+    ]
+
+    for (let id of formValues) {
+        const input = document.getElementById(id.id);
+        if (!input || input.value.trim() === "") {
+            showCustomAlert(id.message);
+            input.focus();
+            return false;
+        }
+    }
+}
